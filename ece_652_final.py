0a7398743bc2888b6e64e4bfaab458541a6fb43a
Huy Trinh <h3trinh@uwaterloo.ca>
2024-07-28 22:33:46 -0400

    Add header description for the file

--- ece_652_final.py
+++ ece_652_final.py
@@ -1,12 +1,16 @@
 # =============================================================================
 #
-# Script Name:
-# Description:
-# Author:
-# Emai:
-# Date:
+# Script Name:      ece_657_final.py
+# Description:      This script will implements a simulator for the Deadline Monotonic
+#                   scheduling algorithm on a single-core CPU. It reads a set of
+#                   periodic tasks from a file, determines if the task set is
+#                   schedulable under the Deadline Monotonic algorithm, and reports
+#                   the number of times each task is preempted per hyperperiod.
+# Author:           Huy Trinh
+# Emai:             h3trinh@uwaterloo.ca
+# Date:             28 Jul, 2024
 # Usage:
-#
+#               python3 ece_621_final.py <worload_file.txt>
 #
 # =============================================================================
 
@@ -16,7 +20,7 @@ import sys
 
 def main():
     if len(sys.argv) != 2:
-        print("[USAGE:] python3 ece_652_final.py workload_test_file")
+        print("[USAGE:] python3 ece_652_final.py <workload_file.txt>")
         return
 
     file_name = sys.argv[1]
