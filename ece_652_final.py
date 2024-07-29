5ae94e390c0ccf82276bb14c1394addd7fb2658f
Huy Trinh <h3trinh@uwaterloo.ca>
2024-07-28 22:57:54 -0400

    Implement calculate_hyperperiod fucntion and add description for functions

--- ece_652_final.py
+++ ece_652_final.py
@@ -17,8 +17,56 @@
 
 import argparse
 import sys
+from math import gcd
+
+
+def LCM(a,b):
+    r"""
+    Calculate the Least Common Multiplier (LCM) between two integers.
+
+
+    Args:
+        a (int) - first integer number
+        b (int) - sencond integer number
+
+    Returns:
+        int: The LCM of the two integer
+    """
+
+    return abs(a*b) // gcd(a,b)
+
+
+
+def calculate_hyperperiod(task_list):
+    r"""
+    Calculate the hyperperiod of a set of tasks
+
+    Args:
+        task_list (list) - A list of tuple, each containing (execution_time, period, deadline) of a task
+
+    Returns
+        int: The hyperperiod of the task set.
+    """
+
+    hyperperiod = task_list[0][1]
+    for _, task_period, _ in task_list[1:]:
+        hyperperiod = LCM(hyperperiod, task_period)
+
+    return hyperperiod
+
+
 
 def get_tasks(filename):
+    r"""
+    Read a file containing task definition and return a list of tasks.
+
+
+    Args:
+        filename(str) - The name of the file containing the tasks
+
+    Returns:
+        list: A list of tuples, each containing (execution_time, period, deadline) of a task
+    """
 
     task_list = []
     with open(filename, 'r') as file:
@@ -32,7 +80,11 @@ def get_tasks(filename):
     return task_list
 
 
+
 def main():
+    r"""
+    Main fuction to execute the Deadline Monotonic scheduling simulator.
+    """
     if len(sys.argv) != 2:
         print("[USAGE:] python3 ece_652_final.py <workload_file.txt>")
         return
@@ -40,7 +92,17 @@ def main():
     fileName = sys.argv[1]
     print(f"file name is: {fileName}")
 
+    # Get the list of tasks from the input file
     task_list = get_tasks(fileName)
+
+    # Cacluate the hyperperiod of task set
+    hyperperiod = calculate_hyperperiod(task_list)
+    print(hyperperiod)
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
