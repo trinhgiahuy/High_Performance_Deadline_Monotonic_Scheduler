9b08a2e17fb1138c41bfce7c60a86baedd62d44a
Huy Trinh <h3trinh@uwaterloo.ca>
2024-07-29 20:58:19 -0400

    Update the Task class and Timeline class's methods

--- task_class.py
+++ task_class.py
@@ -1,9 +1,27 @@
 class Task:
     def __init__(self, id, executiontime, period, deadline):
-        self.id = id
-        self.executiontime = executiontime
         self.period = period
+        self.executiontime = executiontime
         self.deadline = deadline
-        self.next_available = 0
-        self.executed = False
-        self.addedtime = 0      # This is time already executed from current period
+        self.next_available = 0.0
+        self.name = name
+        self.completed = False      # Completed of the task within each of its period
+        self.addedtime = 0.0        # Already executed time in the current period
+        self.preemptions = 0        # Track number of preemptions
+        self. expected_continue = False
+
+    def info(self):
+        return (f"{self.name}, exectime {self.executiontime} next_avai:{self.next_available} executed:{self.completed} addedtime:{self.addedtime} expected_continue: {self.expected_continue} preemptions:{self.preemptions}")
+
+
+    def getName(self):
+        return f"{self.name}"
+
+
+    def getExpectedContinue(self):
+        return self.expected_continue
+
+
+    def getAddedTime(self)
+        return self.executiontime
+
---
+++ timeline_class.py
@@ -0,0 +1,17 @@
+def Timeline:
+    def __init__(self, total_time):
+        self.currenttime = 0.0
+        self.totaltime = 10000
+        self.currenttask = total_time
+        self.tasks = []
+
+
+    def add_task(self,task, duration):
+        fromtime = self.currenttime
+        endtime = fromtime + 1
+        self.tasks.append([task.id, fromtime, endtime])
+        self.currenttime = endtime
+
+
+    def info(self):
+        return (f"TIMELINE: current time: {self.current_time}     tasks: {self.tasks}"
