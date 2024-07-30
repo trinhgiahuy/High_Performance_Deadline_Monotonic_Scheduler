e575259d38b45736d090f313fbb911f538ae8c50
Huy Trinh <h3trinh@uwaterloo.ca>
2024-07-29 21:23:58 -0400

    Fix runtime error and handle unschedulable cases

--- ece_652_final.py
+++ ece_652_final.py
@@ -18,6 +18,8 @@
 import argparse
 import sys
 from math import gcd
+from task_class import Task
+from timeline_class import Timeline
 
 
 def LCM(a,b):
@@ -48,11 +50,15 @@ def calculate_hyperperiod(task_list):
         int: The hyperperiod of the task set.
     """
 
-    hyperperiod = task_list[0][1]
+    print("000")
+    print(task_list)
+    hyperperiod = int(task_list[0][1])
+    print(hyperperiod)
     for _, task_period, _ in task_list[1:]:
-        hyperperiod = LCM(hyperperiod, task_period)
+        hyperperiod = LCM(int(hyperperiod), int(task_period))
 
-    return hyperperiod
+    print(f"Float hyperperiod {float(hyperperiod)}")
+    return float(hyperperiod)
 
 
 
@@ -72,12 +78,45 @@ def get_tasks(filename):
     with open(filename, 'r') as file:
         for line in file:
             if line.strip():
-                task_execution_time, task_period, task_deadline = map(int, line.strip().split(','))
+                task_execution_time, task_period, task_deadline = map(float, line.strip().split(','))
                 print(f"{task_execution_time}, {task_period} {task_deadline}")
                 task_list.append((task_execution_time, task_period, task_deadline))
 
     print(task_list)
     return task_list
+
+
+
+def is_schedulable(tasks):
+    r"""
+    Check if the task set is schedulable under the Deadline Monotonic algorithm.
+
+    Args:
+        tasks (list): List of Task objects
+
+    Returns:
+        bool: True if schedulable, False otherwise
+    """
+
+    utilization = sum(task.executiontime / min(task.deadline, task.period) for task in tasks)
+    return utilization <= 1
+
+
+
+def count_preemptions(task_schedule):
+    preemptions = [0] * len(task_schedule)
+    last_running_task = None
+
+    for task_tuple in task_schedule:
+        if task_tuple != last_running_task and last_running_task is not None:
+            preemptions[task_schedule[0].index(task_tuple)] += 1
+
+        last_running_task = task_tuple
+
+    return preemptions
+
+
+
 
 
 
@@ -99,7 +138,16 @@ def main():
     hyperperiod = calculate_hyperperiod(task_list)
     print(hyperperiod)
 
+    # task_schedule = is_schedulable(task_list, hyperperiod)
+    # preemptions = count_preemptions(task_schedule)
 
+    tasks = [Task(f"T{i}", period, execution_time, deadline) for i, (execution_time, period, deadline) in enumerate(task_list)]
+
+    if is_schedulable(tasks):
+        print("Is schedulable")
+        pass
+    else:
+        print(0)
 
 
 
